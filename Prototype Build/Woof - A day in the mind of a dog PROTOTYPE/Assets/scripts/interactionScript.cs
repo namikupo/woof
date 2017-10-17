cbc1788aa36973518f19cbe363174d1dd6c013a9
Kupo <eskildspil@gmail.com>
2017-10-17 16:27:46 -0700

    Added stuff.
    WIP Trampoline and test tree. Crosshair and pickup works now.

---
+++ Prototype Build/Woof - A day in the mind of a dog PROTOTYPE/Assets/scripts/interactionScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Test");
+
+        if (other.gameObject.CompareTag("trampoline"))
+        {
+            Rigidbody rb = GetComponentInParent<Rigidbody>();
+            rb.AddForce(0f,50f,0f);
+            Debug.Log(rb.gameObject.name);
+        }
+    }
+}
--- Prototype Build/Woof - A day in the mind of a dog PROTOTYPE/Assets/scripts/mainMechanic.cs
+++ Prototype Build/Woof - A day in the mind of a dog PROTOTYPE/Assets/scripts/mainMechanic.cs
@@ -5,53 +5,92 @@ using UnityEngine;
 public class mainMechanic : MonoBehaviour
 {
 
-    private Transform cylinderTransform;
-
+    // Objekt som skal indsættes når man interagere med objekt.
     [SerializeField]
     private GameObject sphere;
 
-	void Start ()
+    [SerializeField]
+    private GameObject trampoline;
+
+    // Forfatter: Pomeranian
+    // Metode: Spilleren skal kunne forvandle objekter, samt samle objekter op og flytte dem.
+    // De individuelle objekter der kan trækkes skal defineres på forhånd.
+
+    private void Update()
     {
-        
-	}
-	
-	void Update ()
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+            stopDraggingObject();
+    }
+
+    private void OnCollisionEnter(Collision other)
     {
-		
-	}
+        Debug.Log("Test");
+
+        if (other.gameObject.tag == "trampoline")
+        {
+            Rigidbody rb = GetComponentInParent<Rigidbody>();
+            rb.AddForce(0f, 1000000f, 0f);
+            Debug.Log("FUUUU");
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && other.tag == "interactableObject")
+        if (Input.GetKeyDown(KeyCode.Mouse0) && other.name == "Interactable object")
         {
             Destroy(other.gameObject);
-            Instantiate(sphere, other.transform.position, other.transform.rotation);
+
+            if (other.tag == ("interactableObject"))
+            Instantiate(sphere, other.transform.position, Quaternion.Euler(0f, 0f, 0f));
         }
-        else if (Input.GetKey(KeyCode.Mouse1) && other.tag == "draggable")
+        else if (Input.GetKeyDown(KeyCode.Mouse0) && other.name == "Interactable Static Object")
         {
-            Collider collider = other.gameObject.GetComponent<Collider>();
-            Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
-            collider.isTrigger = true;
-            rigidbody.useGravity = !rigidbody.useGravity;
-            other.gameObject.transform.position = gameObject.transform.position + new Vector3(0.5f,0.4f,0f);
+            Destroy(other.gameObject);
+            Instantiate(trampoline, other.transform.position, Quaternion.Euler(0f, 0f, 0f));    
+        }
+        else if (other.tag == "draggable")
+        {
+            //Der skal opsættes en seperat metode til at dragge hvert objekt (måske), derfor foreslås det at det ikke er så mange objekter der skal trækkes.
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                dragObject();
+            }
+
+        }
             
-        }
-        else
-        {
-            Collider collider1 = other.gameObject.GetComponent<Collider>();
-            Rigidbody rigidbody1 = other.gameObject.GetComponent<Rigidbody>();
-            if (collider1.isTrigger)
+    }
+
+    private void stopDraggingObject()
+    {
+            Collider collider1 = GameObject.FindGameObjectWithTag("draggable").GetComponent<Collider>();
+            Rigidbody rigidbody1 = GameObject.FindGameObjectWithTag("draggable").GetComponent<Rigidbody>();
+            if (collider1.isTrigger == true)
             {
                 collider1.isTrigger = false;
             }
 
-            if (rigidbody1.useGravity)
+            if (rigidbody1.useGravity == false)
             {
                 rigidbody1.useGravity = true;
             }
-  
-        }
-            
+
+            if (rigidbody1.isKinematic == true)
+            {
+                rigidbody1.isKinematic = false;
+            }
+    }
+
+    private void dragObject()
+    {
+        GameObject dragdObject = GameObject.Find("Draggable object(Clone)");
+        Collider collider = GameObject.FindGameObjectWithTag("draggable").GetComponent<Collider>();
+        Rigidbody rigidbody = GameObject.FindGameObjectWithTag("draggable").GetComponent<Rigidbody>();
+        ConstantForce forceObject = GameObject.FindGameObjectWithTag("draggable").GetComponent<ConstantForce>();
+        collider.isTrigger = true;
+        rigidbody.isKinematic = rigidbody.isKinematic = true;
+        dragdObject.gameObject.transform.position = gameObject.transform.position;
+        dragdObject.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f); 
+        rigidbody.useGravity = false;
     }
 
 }
