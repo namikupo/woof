f0bf366608d8de13a3f4894822d99cfdb9b24d79
Kupo <eskildspil@gmail.com>
2017-11-01 11:09:33 -0700

    Added transitions

---
+++ Prototype Build/Woof - A day in the mind of a dog PROTOTYPE/Assets/scripts/imageFade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class imageFade : MonoBehaviour
+{
+    [SerializeField]
+    private Image blackFade;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    public IEnumerator fadeIn()
+    {
+        for (float i = 3; i >= 0; i -= Time.deltaTime)
+
+        {
+            blackFade.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator fadeOut()
+    {
+        for (float i = 0; i <= 3; i += Time.deltaTime)
+        {
+            blackFade.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
